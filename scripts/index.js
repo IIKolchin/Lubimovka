60c1b739acd5258f6a0fc18f7fac4cb34bee0afc
Apple <IIKolchin@yandex.ru>
2022-07-09 22:16:25 +0300

    create

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+import { initialPhotos, createPhoto } from './utils.js'
+
+const swiperPhoto = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 1,
+  // breakpoints: {
+  //   1440: {
+  //     slidesPerView: 4,
+  //     grid: {
+  //       rows: 2,
+  //     },
+  //   },
+  //   1200: {
+  //     slidesPerView: 3,
+  //     grid: {
+  //       rows: 3,
+  //     },
+  //     spaceBetween: 1,
+  //   },
+    // 300: {
+    //   slidesPerView: 2,
+    //   grid: {
+    //     rows: 4,
+    //   },
+    //   spaceBetween: 1,
+    // },
+  // }
+});
+
+const elementContainer = document.querySelector(".photo__swiper-wrapper");
+
+initialPhotos.forEach((el) => {
+  const photo = createPhoto(el.src);
+  elementContainer.append(photo);
+});
